b70a9c7d3c8a4c99e67fd8034bb2b20473f4e3d0
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2020-05-26 16:42:44 +0200

    verbose

--- generic/2_docker/run_stage.sh
+++ generic/2_docker/run_stage.sh
@@ -8,4 +8,4 @@ cp -f files/*.postinst "${PROFILES}"
 
 sed -i "s/RELEASE/${RELEASE}/g" "${PROFILES}/core.postinst"
 
-(cd ${WORKDIR} && env -i TERM=xterm bash -l -c 'build-simple-cdd --force-root --profiles default --auto-profiles default')
+(cd ${WORKDIR} && env -i TERM=xterm bash -l -c 'build-simple-cdd --force-root --verbose --profiles default --auto-profiles default')
