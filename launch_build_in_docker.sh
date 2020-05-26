1dd9c3010c893a4fc98f6b14f8978da4a18b791f
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2020-05-26 11:50:28 +0200

    rename tmp dir

--- launch_build_in_docker.sh
+++ launch_build_in_docker.sh
@@ -4,9 +4,9 @@ EXTRA_CONFIG_FILE=${1}
 
 docker run --privileged \
           --name nuvlabox-os-build \
-          -v $(pwd):/tmp -w /opt \
+          -v $(pwd):/tmp/nb-os -w /opt \
           -e CONFIG="${EXTRA_CONFIG_FILE}" \
           debian:buster \
-          bash -e -o pipefail -c "cp -fr /tmp/* /opt/ &&
+          bash -e -o pipefail -c "cp -fr /tmp/nb-os/* /opt/ &&
                                   ./build.sh \"${EXTRA_CONFIG_FILE}\" &&
-                                  cp -fr *zip /tmp/"
+                                  cp -fr *zip /tmp/nb-os"
