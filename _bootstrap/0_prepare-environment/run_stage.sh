0b23098d301b76121546266d8923de9b7e182abe
Lionel <lionel@sixsq.com>
2020-09-30 10:53:09 +0200

    Add GPG related code and dependencies.
    Closes #14
    Closes #15
    Closes #17

--- _bootstrap/0_prepare-environment/run_stage.sh
+++ _bootstrap/0_prepare-environment/run_stage.sh
@@ -34,7 +34,7 @@ then
 
   apt-get -y install --no-install-recommends git vim parted quilt coreutils qemu-user-static debootstrap \
                                             zerofree zip dosfstools bsdtar libcap2-bin rsync grep udev xz-utils \
-                                            curl xxd file kmod bc binfmt-support ca-certificates
+                                            curl xxd file kmod bc binfmt-support ca-certificates gnupg
 
   DBOOTSTRAP_BINARY=qemu-debootstrap
 
